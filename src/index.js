6181d2f236c85d6dc2eeb926ceb9b0c72e804b94
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-24 17:27:23 -0700

    add styling

--- src/index.js
+++ src/index.js
@@ -14,14 +14,14 @@ $("form#person-creator").submit(function(event){
   person.venus();
   person.mars();
   person.jupiter(); 
-  $("#mercury").text('Age on Mercury: ' + person.mercuryAge)
-  $("#mercury2").text(person.old + person.mercuryLE + ' Mercurian years.')
+  $("#mercury").text('Age on Mercury: ' + person.mercuryAge);
+  $("#mercury2").text(person.old + person.mercuryLE + ' Mercurian years.');
   $("#venus").text('Age on Venus: ' + person.venusAge);
-  $("#venus2").text( person.old + person.venusLE + ' Venusian years.')
+  $("#venus2").text( person.old + person.venusLE + ' Venusian years.');
   $("#earth").text('Age on Earth: ' + person.age);
-  $("#earth2").text(person.old + person.yearsRem + ' Earth years.')
+  $("#earth2").text(person.old + person.yearsRem + ' Earth years.');
   $("#mars").text('Age on Mars: ' + person.marsAge);
-  $("#mars2").text(person.old + person.marsLE + ' Martian years.')
+  $("#mars2").text(person.old + person.marsLE + ' Martian years.');
   $("#jupiter").text('Age on Jupiter: ' + person.jupiterAge);
-  $("#jupiter2").text(person.old + person.jupiterLE + ' Jovian years.')
+  $("#jupiter2").text(person.old + person.jupiterLE + ' Jovian years.');
 });
